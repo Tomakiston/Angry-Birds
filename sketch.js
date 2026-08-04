96ae0a0cbdb86f8a78b9214700a0d9f55ed78327
Tomakiston <tomasmeme86@gmail.com>
2026-08-04 16:00:27 -0300

    Criação de Classes: BaseClass, Box, Log, Ground

--- sketch.js
+++ sketch.js
@@ -11,6 +11,11 @@ let canvas;
 let backgroundImg;
 let bg;
 
+let ground;
+let platform;
+let box1, box2, box3, box4, box5;
+let log1, log2, log3, log4;
+
 function preload() {
     getBackgroundImg();
 }
@@ -18,13 +23,44 @@ function preload() {
 function setup() {
     canvas = createCanvas(1200, 400);
     canvas.position(15,70);
+
     engine = Engine.create();
     world = engine.world;
+
+    ground = new Ground(600,height, 1200,20);
+    platform = new Ground(150,305, 300,170);
+
+    //Primeiro Andar
+    box1 = new Box(700,320, 70,70);
+    box2 = new Box(920,320, 70,70);
+    log1 = new Log(810,260, 300, PI/2);
+
+    //Segundo Andar
+    box3 = new Box(700,240, 70,70);
+    box4 = new Box(920,240, 70,70);
+    log2 = new Log(810,130, 300, PI/2);
+
+    //Teto
+    box5 = new Box(810,100, 70,70);
+    log3 = new Log(760,70, 150, PI/10);
+    log4 = new Log(870,70, 150, -PI/10);
 }
 
 function draw() {
     background(backgroundImg);
     Engine.update(engine);
+
+    box1.display();
+    box2.display();
+    ground.display();
+    log1.display();
+    box3.display();
+    box4.display();
+    log2.display();
+    box5.display();
+    log3.display();
+    log4.display();
+    platform.display();
 }
 
 function getBackgroundImg() {
